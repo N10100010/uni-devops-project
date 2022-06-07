176547104ef5a5d5fff7a2fb9018e950ad7b46fe
Nick <nr034@hdm-stuttgart.de>
2022-06-07 09:54:40 +0200

    added tests

--- TestApp/main.py
+++ TestApp/main.py
@@ -1,1 +1,2 @@
 from uni_devops_project_app import app
+
---
+++ TestApp/tests/functional.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from uni_devops_project_app import views
+app = Flask(__name__)
+
+
+def test_index_page():
+
+    with app.test_client() as test_client:
+        r = test_client.get('/')
+
+        assert r.status_code == 200
+        assert b"WELCOME" in r.data
